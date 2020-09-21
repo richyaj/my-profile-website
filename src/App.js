f00d50c60c0edaf6096624b4ba2680fa25337619
Richy Ashish Joseph <richyajoe@gmail.com>
2020-09-21 11:31:37 +0530

    Adding page jsx and tried navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React,{Component} from "react";
 import {hot} from "react-hot-loader";
+import MyPage from "./MyPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component{
     render(){
         return(
             <div className="App">
-                <h1>My Portfolio</h1>
+                <MyPage/>
             </div>
         );
     }
---
+++ src/MyPage.jsx
@@ -0,0 +1,18 @@
+import React,{Component} from "react";
+import NavBar from "./NavBar";
+
+class MyPage extends Component{
+    super(props){
+
+    }
+
+    render(){
+        debugger;
+        return(<div>
+                <NavBar/>
+                Richy Ashish Joseph
+            </div>);
+    }
+}
+
+export default MyPage;
